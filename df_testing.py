35f4f21c5583a682593811d32d826d44ea4514b1
Kim.McK <k.mckeever@alumni.unc.edu>
2020-01-22 21:13:25 -0500

    stats change

--- df_testing.py
+++ df_testing.py
@@ -1,36 +1,16 @@
-TIMESTAMP: 30min on 1-15-2020
 import pandas as pd
 import numpy as np
-df=pd.read_csv('donner_party1.csv')
+donner_df=pd.read_csv('donner_party1.csv')
 
-type(df)
-type(df) is a DataFrame
-#I don't know the answer, but I think its false
+members = donner_df.shape[0]
 
-df2=pd.DataFrame.from_csv('donner_party1.csv')
-type(df2)
-type(df2) is a DataFrame
-#I hope this part is true
-#What do you want to do with the dataset now?
-donner_df=
-#donner_df will be which ever is actually a df
+alive= donner_df['Survived'].sum()
+alive_percent=((alive/members).round(4))*100
+dead_percent=(((members-alive)/members).round(4))*100
 
-#good to see the headers. I think its 'Survived', 'sex', 'name', 'age', 'marriage status'
-PartyMembers=len(df)
-#it should be 86
+#What would I like next? Create an if/then clause that would pull the average
+#age of hte survivors and compare it to the averae age of the dead
 
-#Determine amount of survivors, percentage of males/females survived
-Alive=donner_df[['Survived']].sum()
-##should be 48
+#histogram binning by decades of life, double bar demonstrating alive vs. dead by decade
 
-TIMESTAMP: 8:42am to 8:50am on 1-16-2020
-Survived=Alive/PartyMembers
-Deceased=(PartyMembers-Alive)/PartyMembers
-
-#What are the details on the the deceased? Means of death? Consumed?
-#Want to categorize by sex, age of those represnted on the party
-#Narrative for the story of the donner party
-##Timeline of deaths
-##Timeline of rescue
-###They definently ate at least one party member. They also killed to American Indians for their flesh.
-####Gross
+#need to remove the NaN files from the date ranges
